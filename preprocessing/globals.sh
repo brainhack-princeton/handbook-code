8efbabe3f8f864b82782cc2df625f53e81999aaa
Lizzie McDevitt <emcdevitt8287@gmail.com>
2020-12-09 13:41:43 -0500

    update scratch_dir again

--- preprocessing/globals.sh
+++ preprocessing/globals.sh
@@ -11,12 +11,11 @@ scanner_dir=/jukebox/norman/pygers/conquest #Sample project data
 # 2. Edit where your project directory is
 project_dir=/jukebox/YOURLAB/USERNAME/YOURSTUDY
 
-# 3. Edit where your scratch directory is
-scratch_dir=/jukebox/scratch/USERNAME
+# 3. Edit where your scratch and work directories are located (note: make sure you have setup a work directory on scratch)
+scratch_dir=/jukebox/scratch/USERNAME/work/YOURSTUDY
 
 data_dir=$project_dir/data
 bids_dir=$data_dir/bids
 raw_dir=$data_dir/dicom #this is where I want the data from conquest to be copied into my study directory 
 defaced_dir=$bids_dir/derivatives/deface #this is where defaced images will end up
 scripts_dir=$project_dir/code/preprocessing #directory with my preprocessing scripts, including this one
-work_dir=$scratch_dir/work
