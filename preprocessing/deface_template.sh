41fd86903e412571b965056a1b784c5324f60f24
Lizzie McDevitt <emcdevitt8287@gmail.com>
2020-12-09 13:36:49 -0500

    update paths for new version of fmriprep

--- preprocessing/deface_template.sh
+++ preprocessing/deface_template.sh
@@ -3,14 +3,14 @@
 # LOAD GLOBAL VARIABLES AND MODULES ON THE CLUSTER
 source globals.sh   
 module load fsl/6.0.2
-module load pydeface/2.0.0
+module load pydeface/2.0.0 
 
 sid=$1
 
 subj_dir=sub-$sid
 
-T1=$bids_dir/derivatives/fmriprep/$subj_dir/anat/${subj_dir}_desc-preproc_T1w.nii.gz
+T1=$bids_dir/derivatives/fmriprep/$subj_dir/ses-01/anat/${subj_dir}_ses-01_desc-preproc_T1w.nii.gz
 pydeface $T1
 
-T1_defaced=$bids_dir/derivatives/fmriprep/$subj_dir/anat/${subj_dir}_desc-preproc_T1w_defaced.nii.gz
+T1_defaced=$bids_dir/derivatives/fmriprep/$subj_dir/ses-01/anat/${subj_dir}_ses-01_desc-preproc_T1w_defaced.nii.gz
 mv $T1_defaced $defaced_dir
