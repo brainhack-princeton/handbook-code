ad547e1e75dcbbf6966b3f22b9318f7c9aceebf7
Lizzie McDevitt <emcdevitt8287@gmail.com>
2020-12-09 13:42:59 -0500

    update mriqc version

--- preprocessing/run_mriqc.sh
+++ preprocessing/run_mriqc.sh
@@ -3,9 +3,11 @@
 source globals.sh
 
 singularity run --cleanenv \
-    --bind $bids_dir:/home \
-    /jukebox/hasson/singularity/mriqc/mriqc-v0.10.4.sqsh \
-    --participant-label sub-$1 \
+    --bind $bids_dir:/bids \
+    --bind $scratch_dir:/scratch \
+    --bind /usr/people \
+    /jukebox/hasson/singularity/mriqc/mriqc-v0.15.1.simg \
+    --participant-label $1 \
     --correct-slice-timing --no-sub \
-    --nprocs 8 -w /home/derivatives/work \
-    /home /home/derivatives/mriqc participant
+    --nprocs 8 -w /scratch \
+    /bids /bids/derivatives/mriqc participant
--- preprocessing/run_mriqc_group.sh
+++ preprocessing/run_mriqc_group.sh
@@ -3,9 +3,12 @@
 source globals.sh
 
 singularity run --cleanenv \
-    --bind $bids_dir:/home \
-    /jukebox/hasson/singularity/mriqc/mriqc-v0.10.4.sqsh \
+    --bind $bids_dir:/bids \
+    --bind $scratch_dir:/scratch \
+    --bind /usr/people \
+    /jukebox/hasson/singularity/mriqc/mriqc-v0.15.1.simg \
     --correct-slice-timing --modalities T1w bold \
     --no-sub \
-    --nprocs 8 -w /home/derivatives/work \
-    /home /home/derivatives/mriqc group
+    --nprocs 8 -w /scratch \
+    /bids /bids/derivatives/mriqc group
+
